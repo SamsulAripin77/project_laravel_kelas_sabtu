64c067b21ae617fc4fced0c5dc51adbcceeeb90a
SamsulAripin77 <arifins192@gmail.com>
2023-01-18 19:27:15 +0700

    init commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<h1>
+    Halaman Kontact Blade
+</h1>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, praesentium. Mollitia quae saepe, minus aut et culpa veniam qui maiores cupiditate perspiciatis quidem corrupti voluptatum dignissimos. Officia aperiam illum vitae.
+</p>
